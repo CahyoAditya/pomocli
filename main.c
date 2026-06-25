b24973c7f8dc4b12dc111ac7121cbf68f0e33489
CahyoAditya <adityacahyo104@gmail.com>
2026-06-25 18:10:03 +0700

    feat: Add pushNotify OS Based

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+void pushNotify(char* header, char* desc) {
+    char args[150];
+
+    // Push notification OS Based
+    #if defined (__linux__)
+        sprintf(args, "notify-send '%s' '%s'", header, desc);
+    #elif defined(__APPLE__) || defined(__MACH__)
+        sprintf(args, "osascript -e 'display notification \"%s\" with title \"%s\" sound name \"Glass\"'", desc, header);
+    #endif
+
+    system(args);
+}
 
 int main() {
     // focus = Timer for focus
@@ -16,8 +30,27 @@ int main() {
         // timer Focus
         if (state % 2 == 0) {
             sleep(focus * mul);
-            if (state == (fsesi * 2 - 2) && state != 0) printf("Thankyou for your focus! Now take a long rest.\nNote: Long Rest %d sec\n\n", lrest * mul);
-            else printf("Focus is end, Now take some rest!\nNote: Rest %d sec\n\n", rest * mul);
+
+            // Logic for long rest
+            if (state == (fsesi * 2 - 2) && state != 0) {
+                char header[50] = "Thankyou for your focus!";
+                char desc[100];
+                sprintf(desc, "Now take a long rest. Note: Long Rest %d sec", lrest * mul);
+
+                printf("%s\n%s\n\n", header, desc);
+                pushNotify(header, desc);
+            }
+
+            // Logic for normal rest
+            else {
+                char header[50] = "Focus is end!";
+                char desc[100];
+                sprintf(desc, "Now take some rest. Note: Rest %d sec", rest * mul);
+
+                printf("%s\n%s\n\n", header, desc);
+                pushNotify(header, desc);
+            }
+
             state++;
         }
 
@@ -26,14 +59,26 @@ int main() {
             // normal Rest
             if (state % (fsesi * 2 - 1) != 0) {
                 sleep(rest * mul);
-                printf("Rest Time's Up!\nNote: Focus %d sec\n\n", focus * mul);
+
+                char header[50] = "Rest Times Up!";
+                char desc[100];
+                sprintf(desc, "Note: Focus %d sec", focus * mul);
+
+                printf("%s\n%s\n\n", header, desc);
+                pushNotify(header, desc);
                 state++;
             }
 
             // long Rest
             else {
                 sleep(lrest * mul);
-                printf("Rest Time's Up!\nNote: Focus %d sec\n\n", focus * mul);
+
+                char header[50] = "Rest Times Up!";
+                char desc[100];
+                sprintf(desc, "Note: Focus %d sec", focus * mul);
+
+                printf("%s\n%s\n\n", header, desc);
+                pushNotify(header, desc);
                 state = 0;
             }
         }
