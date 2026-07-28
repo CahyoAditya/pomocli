7fedef92ece99e8d6e1c1131bd00a22ac3016d3f
CahyoAditya <adityacahyo104@gmail.com>
2026-07-28 09:54:35 +0700

    refactor: make func runFocus and runRest for readability

--- main.c
+++ main.c
@@ -28,8 +28,36 @@ void notify(char* header, char* desc) {
     pushNotify(header, desc);
 }
 
+// Function to run the focus timer
+void runFocus(int state, int focus, int rest, int lrest, int mul, int fsesi) {
+    sleep(focus * mul);
+
+    char desc[100];
+    if (state == (fsesi * 2 - 2) && state != 0) {
+        sprintf(desc, "Now take a long rest. Note: Long Rest %d sec", lrest * mul);
+        notify("Thankyou for your focus!", desc);
+    } else {
+        sprintf(desc, "Now take some rest. Note: Rest %d sec", rest * mul);
+        notify("Focus is end!", desc);
+    }
+}
+
+// Function to run the rest timer
+void runRest(int state, int focus, int rest, int lrest, int mul, int fsesi) {
+    char desc[100];
+    sprintf(desc, "Note: Focus %d sec", focus * mul);
+
+    if (state % (fsesi * 2 - 1) != 0) {
+        sleep(rest * mul);
+    } else {
+        sleep(lrest * mul);
+    }
+
+    notify("Rest Times Up!", desc);
+}
+
 int main() {
-    int focus = 25, rest = 5, lrest = 15, mul = 1, fsesi = 4;
+    int focus = 10, rest = 5, lrest = 15, mul = 1, fsesi = 4;
     int state = 0;
 
     printf("Timer start %d sec!\n\n", focus * mul);
@@ -37,32 +65,13 @@ int main() {
     while (1){
         // timer Focus
         if (state % 2 == 0) {
-            sleep(focus * mul);
-
-            char desc[100];
-            if (state == (fsesi * 2 - 2) && state != 0) {
-                sprintf(desc, "Now take a long rest. Note: Long Rest %d sec", lrest * mul);
-                notify("Thankyou for your focus!", desc);
-            } else {
-                sprintf(desc, "Now take some rest. Note: Rest %d sec", rest * mul);
-                notify("Focus is end!", desc);
-            }
-
+            runFocus(state, focus, rest, lrest, mul, fsesi);
             state++;
         }
 
         // timer Rest
         else {
-            char desc[100];
-            sprintf(desc, "Note: Focus %d sec", focus * mul);
-
-            if (state % (fsesi * 2 - 1) != 0) {
-                sleep(rest * mul);
-            } else {
-                sleep(lrest * mul);
-            }
-
-            notify("Rest Times Up!", desc);
+            runRest(state, focus, rest, lrest, mul, fsesi);
 
             if (state % (fsesi * 2 - 1) != 0) {
                 state++;
